615bbda6164a287e94bd2d728c4d30df05bd6b18
trjohnso <trjohnso@mymail.mines.edu>
2015-01-31 17:34:24 -0700

    Reconciled nose/ear functionality. Would not merger correctly. Merged by hand and repushed into repo. Most likly a change in the mouth variables needed for nose functionality.

--- src/Cat.java
+++ src/Cat.java
@@ -18,8 +18,13 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/3;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 3;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 4;
+	// nose dimensions
+	private static final int NOSE_DIAMETER = HEAD_DIMENSION/5;
+	//nose starts in middle of face.
+	private static final int NOSE_X = HEAD_DIMENSION/2 - NOSE_DIAMETER/2;
+	private static final int NOSE_Y = HEAD_DIMENSION/2 - NOSE_DIAMETER/4;
 	//ear dimensions
 	private static final int EAR_WIDTH = HEAD_DIMENSION/5;
 	private static final int EAR_HEIGHT = 50;
@@ -52,6 +57,11 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		//draw the nose
+		g2.setColor(Color.green);
+		x = catX + NOSE_X;
+		y = catY + NOSE_Y;
+		g2.fillOval(x,y, NOSE_DIAMETER, NOSE_DIAMETER);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
